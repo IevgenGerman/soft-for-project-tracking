724e3a2f1dab698c779c9fca736e24d8b4348551
germa <german.kbrza@gmail.com>
2026-08-04 21:43:44 +0300

    feat(static): configure static files handling with WhiteNoise
    - Add STATIC_ROOT settings in settings.py
    - Add WhiteNoise middleware for serving static files in production
    - Add .gitkeep to staticfiles directory to preserve folder structure
    - Update .gitignore to ignore collected static files except .gitkeep

---
+++ tracking_project/settings/production.py
@@ -0,0 +1,17 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": int(os.environ.get("POSTGRES_DB_PORT")),
+    }
+}
